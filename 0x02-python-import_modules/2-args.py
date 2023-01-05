c4f7bd04c92a28cfa7398ebe07e8cc581a6104cc
Bobson Obiku <leonineobiku@gmail.com>
2023-01-05 05:21:04 -0800

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,11 +4,10 @@ from sys import argv
 if __name__ == "__main__":
     counter = len(argv) - 1
     if counter == 0:
-        print("{} arguments:".format(counter))
+        print("{} arguments.".format(counter))
     elif counter == 1:
         print("{} argument:".format(counter))
-        print("{}: ".format(counter) + argv[1])
     else:
         print("{} arguments:".format(counter))
-    for i in range(1, len(argv[i])):
-        print('{}: {}'.format(i, argv[i]))
+    for i in range(counter):
+        print('{}: {}'.format(i + 1, argv[i + 1]))
