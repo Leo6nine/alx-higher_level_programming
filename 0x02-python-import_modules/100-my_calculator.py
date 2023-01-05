36ab563ddf01995dbe9880a559d4606b08263602
Bobson Obiku <leonineobiku@gmail.com>
2023-01-05 15:50:22 -0800

    Task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,27 +3,23 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    x = argv[1]
-    y = argv[3]
-    a = int(x)
-    b = int(y)
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        if argv[2] not in ["+", "-", "*", "/"]:
-            print("Unknown operater. Available poerators: +, -, *, and /")
-            exit (1)
+        if argv[2] not in ["+", "/", "-", "*"]:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
         else:
             if argv[2] == '+':
-                tmp = add(a, b)
-                print("{} + {} = {}".format(x, y, tmp))
+                tmp = add(int(argv[1]), int(argv[3]))
+                print("{} + {} = {}".format(argv[1], argv[3], tmp))
             elif argv[2] == '-':
-                tmp = sub(a, b)
-                print("{} - {} = {}".format(x, y, tmp))
+                tmp = sub(int(argv[1]), int(argv[3]))
+                print("{} - {} = {}".format(argv[1], argv[3], tmp))
             elif argv[2] == '*':
-                tmp = mul(a, b)
-                print("{} * {} = {}".format(x, y, tmp))
+                tmp = mul(int(argv[1]), int(argv[3]))
+                print("{} * {} = {}".format(argv[1], argv[3], tmp))
             elif argv[2] == '/':
-                tmp = div(a, b)
-                print("{} / {} = {}".format(x, y, tmp))
+                tmp = div(int(argv[1]), int(argv[3]))
+                print("{} / {} = {}".format(argv[1], argv[3], tmp))
