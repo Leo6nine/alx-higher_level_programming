b2a58b62c9f3e1a82f29bfaa2632a2ef499f904b
Bobson Obiku <leonineobiku@gmail.com>
2023-01-05 06:07:39 -0800

    Second commit

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        exit(1)
+
+    op = argv[2]
+    sign = {"+": add, "-": sub, "*": mul, "/": div}
+    if op not in sign:
+        print("Unknown operater. Available poerators: +, -, *, and /")
+        exit (1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    print("{:d} {:s} {:d} = {:d}".format(a, op, b, sign[op](a, b)))
