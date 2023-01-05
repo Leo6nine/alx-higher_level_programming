0c93fd932cb3e220bfa19015cf1eb29718e0fab5
Bobson Obiku <leonineobiku@gmail.com>
2023-01-05 06:22:34 -0800

    Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,13 +6,12 @@ if __name__ == "__main__":
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
+        
     op = argv[2]
     sign = {"+": add, "-": sub, "*": mul, "/": div}
     if op not in sign:
         print("Unknown operater. Available poerators: +, -, *, and /")
         exit (1)
-
     a = int(argv[1])
     b = int(argv[3])
     print("{:d} {:s} {:d} = {:d}".format(a, op, b, sign[op](a, b)))
